947f81fbaf15f90b3089cba258c9d91a22be9c8c
zhuzzzhha <90785698+zhuzzzhha@users.noreply.github.com>
2022-10-30 18:10:11 +0300

    Update c_queue_p.c

--- c_queue_p.c
+++ c_queue_p.c
@@ -1,7 +1,41 @@
-ErrorCode CreateQueueC(QueueHandler* queue);
-ErrorCode CloneQueueC(QueueHandler source, QueueHandler* queue);
-ErrorCode DestroyQueueC(QueueHandler queue);
-ErrorCode PopC(QueueHandler queue);
-ErrorCode PushC(QueueHandler queue, int32_t value);
-ErrorCode IsEmptyC(QueueHandler queue, int32_t* result);
-ErrorCode TopC(QueueHandler queue, int32_t* result);
+typedef uint64_t QueueHandler;
+
+   ErrorCode CreateQueue(QueueHandler* queue)
+   {
+   	
+   };
+
+   ErrorCode CloneQueue(QueueHandler source, QueueHandler* queue)
+   {
+   	
+   };
+
+   ErrorCode DestroyQueue(QueueHandler* queue)
+   {
+   	
+   };
+
+   ErrorCode Pop(QueueHandler queue)
+   {
+   	
+   };
+
+   ErrorCode Push(QueueHandler queue, int32_t value)
+   {
+   	
+   };
+
+   ErrorCode IsEmpty(QueueHandler queue, int32_t* result)
+   {
+   	
+   };
+
+   ErrorCode Top(QueueHandler queue, int32_t* result)
+   {
+   	
+   };
+
+   const char* WhatIs(ErrorCode err)
+   {
+   	
+   };
