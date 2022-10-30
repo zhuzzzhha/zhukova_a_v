c74b456db5063e0b91cbdf653e845c5b7cc40901
zhuzzzhha <90785698+zhuzzzhha@users.noreply.github.com>
2022-10-30 18:07:17 +0300

    Update queue_p.h

--- queue_p.h
+++ queue_p.h
@@ -6,7 +6,7 @@
 #include <memory>
 #include <vector>
 #include <algorithm>
-struct Queue {
+struct QueueP {
 private:
 	struct Node {
 		int data;
@@ -22,86 +22,23 @@ private:
 	};
 	std::unique_ptr<Node> head;
 public:
-	~Queue()
+	~QueueP()
 	{
 		while (head)
 			head = std::move(head->next);
 	}
-	Queue() :head() {};
-	Queue(const Queue& copy) = default;
-	void push(const int value)
-	{
-		if (this->head.get() == nullptr)
-		{
-			std::unique_ptr<Node> new_value = std::make_unique<Node>();
-			new_value->data = value;
-			new_value->next = nullptr;
-			head = std::move(new_value);
-		}
-		else {
-			Node* prev = head.get();
-			while (prev->next!=nullptr && value <= prev->next->data)
-			{
-				prev = (prev->next).get();
-			}
-			std::unique_ptr<Node> new_value = std::make_unique<Node>();
-			new_value->data = value;
-			new_value->next = std::move(prev->next);
-			prev->next = std::move(new_value);
-		}
-	}
-	void pop()
-	{
-		head = std::move(head->next);
-	}
-	int top()
-	{
-		return head->data;
-	}
-	bool operator==(const Queue& rhs) const
-	{
-		Node* left = head.get();
-		Node* right = rhs.head.get();
-		while (left != nullptr && right != nullptr)
-		{
-			if (left->data != right->data)
-				return false;
-			left = (left->next).get();
-			right = (right->next).get();
-
-		}
-		return true;
-
-	}
-	Queue& Queue::operator=(const Queue& rhs) {
-		*this = rhs;
-		return *this;
-	}
-	/*std::ostream& operator<<(std::ostream& ostream, const Queue& temp)
-	{
-			while (temp->next != nullptr)
-			{
-				ostream << temp.top()<<' ';
-				temp.pop();
-			}
-		return ostream;
-	}
-	std::istream& operator>>(std::istream& istream, Queue& temp)
-	{
-		std::vector<int> c;
-		for (int i = 0; i < c.size(); i++)
-		{
-			istream >> c[i];
-			temp.push(c[i]);
-		}
-		return istream;
-	}*/
+	QueueP() :head() {};
+	QueueP(QueueP&&) noexcept = default;
+	QueueP(const QueueP& copy) = default;
+	void push(const int& value);
+	void pop() noexcept;
+	const int& top() const;
+	bool operator==(const QueueP& rhs) const;
+	QueueP& operator=(const QueueP& rhs)=default;
+	QueueP& operator=(QueueP&& other) = default;
+	
 };
-std::ostream& operator<<(std::ostream& ostrm, const Queue& rhs);
-
-std::istream& operator>>(std::istream& istrm, Queue& rhs);
 
 
 
 #endif
-
