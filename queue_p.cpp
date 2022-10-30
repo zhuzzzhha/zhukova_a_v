212834ac89de183b03f9f4e513e49338efa4403f
zhuzzzhha <90785698+zhuzzzhha@users.noreply.github.com>
2022-10-30 18:06:55 +0300

    Update queue_p.cpp

--- queue_p.cpp
+++ queue_p.cpp
@@ -7,5 +7,52 @@
 #include <memory>
 #include <vector>
 #include <algorithm>
-#include<queuep/queuep.h>
+#include<queue_p/queue_p.h>
+void QueueP::push(const int& value)
+{
+	if (this->head.get() == nullptr)
+	{
+		std::unique_ptr<Node> new_value = std::make_unique<Node>();
+		new_value->data = value;
+		new_value->next = nullptr;
+		head = std::move(new_value);
+	}
+	else {
+		Node* prev = head.get();
+		while (prev->next != nullptr && value <= prev->next->data)
+		{
+			prev = (prev->next).get();
+		}
+		std::unique_ptr<Node> new_value = std::make_unique<Node>();
+		new_value->data = value;
+		new_value->next = std::move(prev->next);
+		prev->next = std::move(new_value);
+	}
+}
+void QueueP::pop() noexcept
+{
+	head = std::move(head->next);
+}
+const int& QueueP::top() const
+
+{
+	return head->data;
+}
+bool QueueP::operator==(const QueueP& rhs) const
+{
+	Node* left = head.get();
+	Node* right = rhs.head.get();
+	while (left != nullptr && right != nullptr)
+	{
+		if (left->data != right->data)
+			return false;
+		left = (left->next).get();
+		right = (right->next).get();
+
+	}
+	return true;
+}
+
+
+
 #endif
