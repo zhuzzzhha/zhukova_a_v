dab3844eae7bb5690e0b82c79bc2f202093bac24
zhuzzzhha <90785698+zhuzzzhha@users.noreply.github.com>
2022-11-13 17:20:34 +0300

    Update c_queue_p.c

--- c_queue_p.c
+++ c_queue_p.c
@@ -1,4 +1,15 @@
-typedef uint64_t QueueHandler;
+#include<cpp_manager/cpp_manager.h>
+#include <c_queue_p/c_queue_p.h>
+#pragma once
+
+
+ErrorCode CreateQueueC(QueueHandler* queue);
+ErrorCode CloneQueueC(QueueHandler source, QueueHandler* queue);
+ErrorCode DestroyQueueC(QueueHandler queue);
+ErrorCode PopC(QueueHandler queue);
+ErrorCode PushC(QueueHandler queue, int32_t value);
+ErrorCode IsEmptyC(QueueHandler queue, int32_t* result);
+ErrorCode TopC(QueueHandler queue, int32_t* result);
 
    ErrorCode CreateQueue(QueueHandler* queue)
    {
